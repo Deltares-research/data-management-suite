570b79aea84dcfb6f7913db6656e05561326af63
Nathan Clerkx <nathan@wolk.work>
2023-08-21 14:01:01 +0000

    add azd project with terraform provider

---
+++ infra/provider.tf
@@ -0,0 +1,12 @@
+# Configure the Azure Provider
+terraform {
+  required_version = ">= 1.1.7, < 2.0.0"
+  required_providers {
+    azurerm = {
+      version = "~>3.47.0"
+      source  = "hashicorp/azurerm"
+    }
+  }
+  backend "azurerm" {
+  }
+}
