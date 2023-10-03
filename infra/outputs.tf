455af02431dcf66ee8cc8a46ef9a8ae6b875a711
Nathan Clerkx <nathan@wolk.work>
2023-10-03 10:08:47 +0200

    add resource group to output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,7 @@
 output "AZURE_CONTAINER_REGISTRY_ENDPOINT" {
   value = module.container_app.container_registry_server
 }
+
+output "AZURE_RESOURCE_GROUP" {
+  value = azurerm_resource_group.rg.name
+}
