dd9850ece59763c62965c025e681f3ccb44c9405
Nathan Clerkx <nathan@wolk.work>
2023-11-06 16:25:38 +0100

    add coverage badge

--- utils/deltares_datasuite/tests/test_item.py
+++ utils/deltares_datasuite/tests/test_item.py
@@ -1,6 +1,6 @@
 from pystac import Item
 
-from deltares_datasuite.core.models.item import DataManagementSuiteItem
+from deltares_datasuite.core import DataManagementSuiteItem
 
 
 def test_item(item: Item):
