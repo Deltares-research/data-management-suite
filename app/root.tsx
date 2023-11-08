7aa582c9c6a1892d76c9e0d9aee0005d5356043a
Robert <robertbroersma@gmail.com>
2023-11-08 19:27:19 +0100

    Add sidebar menu for CMS

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,31 @@
 import { cssBundleHref } from '@remix-run/css-bundle'
-import type { LinksFunction } from '@remix-run/node'
+import type { ActionArgs, LinksFunction, LoaderArgs } from '@remix-run/node'
+import type { NavLinkProps } from '@remix-run/react'
 import {
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react'
 
 import mapboxStyles from 'mapbox-gl/dist/mapbox-gl.css'
 import mapboxDrawStyles from '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 import styles from './tailwind.css'
+import { Logo } from './components/Logo'
+import { routes } from './routes'
+import { createAuthenticator } from './services/auth.server'
+import { db } from './utils/db.server'
+import { Avatar, AvatarFallback } from './components/ui/avatar'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './components/ui/tooltip'
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: styles },
@@ -20,7 +34,31 @@ export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 ]
 
+export async function rootLoader({ request }: LoaderArgs) {
+  let authenticator = createAuthenticator(request)
+  let user = await authenticator.isAuthenticated(request)
+
+  return db.person.findUnique({
+    where: {
+      id: user?.id ?? '',
+    },
+    include: {
+      memberOf: true,
+    },
+  })
+}
+
+export let loader = rootLoader
+
+export async function action({ request }: ActionArgs) {
+  let authenticator = createAuthenticator(request)
+  await authenticator.logout(request, { redirectTo: routes.login() })
+}
+
 export default function App() {
+  let user = useLoaderData<typeof loader>()
+  let [firstName, lastName] = (user?.name ?? '? ?').split(' ')
+
   return (
     <html lang="en" className="h-full">
       <head>
@@ -30,11 +68,54 @@ export default function App() {
         <Links />
       </head>
       <body className="flex flex-col h-full">
-        <Outlet />
+        <div className="h-full flex flex-col">
+          <div className="border-b flex-shrink-0 h-12 px-8 flex items-center gap-12">
+            <Logo className="h-4" />
+            <div className="flex gap-5 items-center py-2">
+              <MenuItem to={routes.home()}>Home</MenuItem>
+              <MenuItem to={routes.search()}>Search</MenuItem>
+              <MenuItem to={routes.items()}>CMS</MenuItem>
+            </div>
+
+            {user && (
+              <TooltipProvider delayDuration={0}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Avatar className="ml-auto">
+                      <AvatarFallback>
+                        {firstName?.[0]}
+                        {lastName?.[0]}
+                      </AvatarFallback>
+                    </Avatar>
+                  </TooltipTrigger>
+                  <TooltipContent align="end">
+                    Logged in as {user?.name}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+          </div>
+          <Outlet />
+        </div>
+
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  )
+}
+
+function MenuItem(props: NavLinkProps) {
+  return (
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    <NavLink
+      className={({ isActive }) =>
+        `text-sm font-medium transition-colors hover:text-primary ${
+          isActive ? 'text-primary' : 'text-muted-foreground'
+        }`
+      }
+      {...props}
+    />
   )
 }
